2ec2bc118d111cf87d2bd49dc58e5fbb8bb02af8
AshishUpre <ashishupre1134@gmail.com>
2025-03-01 19:26:43 +0530

    Adding oauth2, about to remove jwt

--- src/main/java/com/ashupre/whatsappparser/config/EnvConfig.java
+++ src/main/java/com/ashupre/whatsappparser/config/EnvConfig.java
@@ -1,13 +1,31 @@
 package com.ashupre.whatsappparser.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EnvConfig {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    private final Dotenv dotenv = Dotenv.load();
+
+    /**
+     * manually setting oauth credentials
+     * # get from console.cloud.google.com -> create new project / existing project -> left panel -> APIs & Services
+     * # -> Credentials -> + create credentials (at top)
+     * # first you need to configure consent screen for that
+     * # whether using for internal or external -> if chosen internal only Google Workspace accounts can log in (companies buying)
+     * # external -> users can use their gmail account
+     */
+    @PostConstruct
+    public void loadOauthCredentials() {
+        // these lines must be present in application.properties
+        // spring.security.oauth2.client.registration.google.client-id=${GOOGLE_CLIENT_ID}
+        // spring.security.oauth2.client.registration.google.client-secret=${GOOGLE_CLIENT_SECRET}
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+    }
 
     @Bean
     public String folderId() {
@@ -37,4 +55,6 @@ public class EnvConfig {
     public String jwtSecret() {
         return dotenv.get("JWT_SECRET");
     }
+
+
 }
